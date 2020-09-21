68c483851bfe1aac21d2dd5efee5a0aecff8a066
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-09-21 13:26:50 +0300

    feat: Init project

---
+++ src/main/java/com/example/pivottable/PivotTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.pivottable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivotTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PivotTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pivottable/PivotTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pivottable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivotTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
